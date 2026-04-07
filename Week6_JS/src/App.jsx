42c7b129bcd3eec1142e243a12d250968d8d2e3a
Le Thi Kim Ngan <23729131.ngan@student.iuh.edu.vn>
2026-04-07 19:07:56 +0700

    complete excercise 2

--- Week6_JS/src/App.jsx
+++ Week6_JS/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, BrowserRouter, Link } from "react-router-dom"
 import Home from "./components/Home"
 import About from "./components/About"
 import Contact from "./components/Contact"
+import NotFound from "./components/NotFound"
 
 const App = () => {
   return (
@@ -18,6 +19,9 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
+
+
+          <Route path="*" element={<NotFound />} />
         </Routes>
 
 
---
+++ Week6_JS/src/components/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NotFound() {
+    return (
+        <div style={{ color: 'red', fontSize: '24px' }}>
+            404 Not Found
+        </div>
+    )
+}
+
+export default NotFound
