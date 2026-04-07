62c8f4f06d4950d6f62759af1cce60e2ede2ed48
Le Thi Kim Ngan <23729131.ngan@student.iuh.edu.vn>
2026-04-07 19:40:31 +0700

    complete bai 3

--- Week6_JS/src/App.jsx
+++ Week6_JS/src/App.jsx
@@ -3,29 +3,46 @@ import Home from "./components/Home"
 import About from "./components/About"
 import Contact from "./components/Contact"
 import NotFound from "./components/NotFound"
+import ProductDetail from "./components/ProductDetail"
 
 const App = () => {
+  const items = ['Iphone', 'Samsung', 'Laptop']
   return (
+    // <BrowserRouter>
+    //   <div>
+
+    //     <div className="app">
+    //       <Link to="/">Home</Link>
+    //       <Link to="/about">About</Link>
+    //       <Link to="/contact">Contact</Link>
+    //     </div>
+
+    //     <Routes>
+    //       <Route path="/" element={<Home />} />
+    //       <Route path="/about" element={<About />} />
+    //       <Route path="/contact" element={<Contact />} />
+    //       <Route path="*" element={<NotFound />} />
+    //     </Routes>
+
+
+    //   </div>
+
+    // </BrowserRouter>
+
+
+
     <BrowserRouter>
       <div>
-
-        <div className="app">
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/contact">Contact</Link>
-        </div>
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-
-
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-
-
+        {items.map((item, index) => {
+          return (
+            <Link key={index} to={`/products/${index}`}>Product {index}</Link>
+          )
+        })}
       </div>
+      <Routes>
+        <Route path="/products/:id" element={<ProductDetail />}></Route>
+      </Routes>
+
 
     </BrowserRouter>
 
