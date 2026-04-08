cdc2b967b1772131d9a75b25f958a8e1d099942e
H82M19\Student <Student>
2026-04-08 10:27:42 +0700

    complete bai 2

--- Week7_JS/src/App.jsx
+++ Week7_JS/src/App.jsx
@@ -5,10 +5,14 @@ import heroImg from './assets/hero.png'
 import './App.css'
 import { useRecoilState } from 'recoil'
 import { counterAtom } from './components/CounterA'
+import { themeAtom } from './components/Theme'
+
 
 function App() {
  
   const [counter, setCounter] = useRecoilState(counterAtom)
+
+ 
 
   const handleIncrement = () => {
     setCounter (counter + 1)
@@ -16,12 +20,24 @@ function App() {
   const handleDecrement = () => {
     setCounter (counter > 0 ? counter - 1 : 0)
   }
+  
+  const [theme, setTheme] = useRecoilState (themeAtom)
+
+  const handleTheme = () => {
+    setTheme (theme === 'light' ? 'dark' : 'light')
+    document.body.style.background = theme === 'light' ? 'black' : 'white'
+    document.body.style.color = theme === 'light' ? 'black' : 'white'
+  }
 
   return (
     <>
       <h2>Counter: {counter}</h2>
       <button onClick={handleIncrement}>Increment</button>
       <button onClick={handleDecrement}>Decrement</button>
+
+      <button onClick={handleTheme}>{theme}</button>
+
+     
     </>
   )
 }
